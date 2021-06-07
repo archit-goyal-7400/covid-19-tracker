7c20c3d1b2c046d8aa164bf1908d033c46889388
archit828 <garchit2000@gmail.com>
2021-06-07 11:59:40 +0530

    deploying

--- client.js
+++ client.js
@@ -9,15 +9,11 @@ app.get("/", function (req, res) {
   res.json({ version: packageInfo.version });
 });
 
-var server = app.listen(process.env.PORT, "0.0.0.0", () => {
-  const host = server.address().address;
-  const port = server.address().port;
-  console.log("Web server started at http://%s:%s", host, port);
-});
+var server = app.listen(process.env.PORT);
 
 module.exports = (bot) => {
   app.post("/" + bot.token, (req, res) => {
-    bot.processUpdate(req.body);
+    bot.handleUpdate(req.body);
     res.sendStatus(200);
   });
 };
